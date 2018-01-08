b18236c317ef12b08f3643989974fed9d14d529b
Vagner Guedes <rj.vag.ner@gmail.com>
2018-01-08 20:31:26 -0200

    Conectando o Django à um postgres.

--- django_api/reclameaqui/reclameaqui/settings.py
+++ django_api/reclameaqui/reclameaqui/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'reclameaqui.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'reclameaqui_api',
+        'USER': 'postgres',
+        'PASSWORD': 'asdqwe123',
+        'HOST': 'postgres',
+        'PORT': ''
     }
 }
 
