16a7ca50db94c301c10afa74c98c363a1b0e1663
Vagner Guedes <rj.vag.ner@gmail.com>
2018-01-16 02:06:26 -0200

    Basic AUTH na API.

--- django_api/inventory/views.py
+++ django_api/inventory/views.py
@@ -15,33 +15,40 @@ from inventory.serializers import (
     ComplainSerializer
 )
 from rest_framework import viewsets
+from rest_framework import permissions
 
 
 class CountryViewSet(viewsets.ModelViewSet):
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class StateViewSet(viewsets.ModelViewSet):
     serializer_class = StateSerializer
     queryset = State.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class CityViewSet(viewsets.ModelViewSet):
     serializer_class = CitySerializer
     queryset = City.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class ConsumerViewSet(viewsets.ModelViewSet):
     serializer_class = ConsumerSerializer
     queryset = Consumer.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class ComplainViewSet(viewsets.ModelViewSet):
     serializer_class = ComplainSerializer
     queryset = Complain.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
