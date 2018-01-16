8f771a42bcac7600ebfd4b9b1ed85b65c504a0fa
Vagner Guedes <rj.vag.ner@gmail.com>
2018-01-16 01:29:37 -0200

    RESTFULL API.

--- django_api/inventory/views.py
+++ django_api/inventory/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from inventory.models import (
+    Country,
+    State,
+    City,
+    Consumer,
+    Company,
+    Complain
+)
+from inventory.serializers import (
+    CountrySerializer,
+    StateSerializer,
+    CitySerializer,
+    ConsumerSerializer,
+    CompanySerializer,
+    ComplainSerializer
+)
+from rest_framework import viewsets
 
-# Create your views here.
+
+class CountryViewSet(viewsets.ModelViewSet):
+    serializer_class = CountrySerializer
+    queryset = Country.objects.all()
+
+
+class StateViewSet(viewsets.ModelViewSet):
+    serializer_class = StateSerializer
+    queryset = State.objects.all()
+
+
+class CityViewSet(viewsets.ModelViewSet):
+    serializer_class = CitySerializer
+    queryset = City.objects.all()
+
+
+class ConsumerViewSet(viewsets.ModelViewSet):
+    serializer_class = ConsumerSerializer
+    queryset = Consumer.objects.all()
+
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all()
+
+
+class ComplainViewSet(viewsets.ModelViewSet):
+    serializer_class = ComplainSerializer
+    queryset = Complain.objects.all()
