f1488175b93eb5ab89c56fb8e47e8a29e37c85d5
Vagner Guedes <vguedes@localhost.localdomain>
2018-01-24 20:28:35 -0200

    permissions

--- django_api/inventory/views.py
+++ django_api/inventory/views.py
@@ -1,6 +1,7 @@
 """API Views"""
 
 from rest_framework import viewsets
+from rest_framework import permissions
 from inventory.models import (
     Country,
     State,
@@ -22,28 +23,34 @@ from inventory.serializers import (
 class CountryViewSet(viewsets.ModelViewSet):
     serializer_class = CountrySerializer
     queryset = Country.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class StateViewSet(viewsets.ModelViewSet):
     serializer_class = StateSerializer
     queryset = State.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class CityViewSet(viewsets.ModelViewSet):
     serializer_class = CitySerializer
     queryset = City.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class ConsumerViewSet(viewsets.ModelViewSet):
     serializer_class = ConsumerSerializer
     queryset = Consumer.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
 
 
 class ComplainViewSet(viewsets.ModelViewSet):
     serializer_class = ComplainSerializer
     queryset = Complain.objects.all()
+    permission_classes = (permissions.IsAuthenticated,)
